4583673b00d593c90d9dbc94d159697c94a137fd
danielcanoh <daniel.canoh22@gmail.com>
2022-09-07 16:59:36 -0500

    Arrays :rocket:

--- 08-arrays-Bankist/script.js
+++ 08-arrays-Bankist/script.js
@@ -61,6 +61,163 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const createUsernames = function (accs) {
+  accs.forEach(acc => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+
+const displayMovements = function (movements, sort = false) {
+  containerMovements.innerHTML = '';
+
+  const movs = sort ? [...movements].sort((a, b) => a - b) : movements;
+
+  movs.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__date">3 days ago</div>
+      <div class="movements__value">${mov}€</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(mov => mov * (acc.interestRate / 100))
+    .filter(int => int >= 1)
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumIn.textContent = `${incomes}€`;
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+};
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+// Event handler
+let currentAccount;
+
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner
+      .split(' ')
+      .at(0)}`;
+    containerApp.style.opacity = 1;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', e => {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    amount <= currentAccount.balance &&
+    receiverAcc &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    updateUI(currentAccount);
+  }
+});
+
+btnLoan.addEventListener('click', e => {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (sorted) displayMovements(currentAccount.movements);
+  else displayMovements(currentAccount.movements, true);
+  sorted = !sorted;
+})
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // TODO LECTURES
@@ -71,10 +228,10 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 console.log('SLICE');
@@ -104,3 +261,127 @@ console.log([...arr, ...arr2]);
 console.log('\nJOIN');
 // Convertir un array en un string
 console.log(letters.join(' - '));
+
+console.log('\nAT');
+// Mostrar un elemento del array
+const arr3 = [11, 12, 22, 25];
+console.log(arr3.at(0));
+console.log(arr3.at(-1));
+
+console.log('\nFOR EACH');
+// For each permite recorrer un array y ejecutar una función por cada elemento
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// // Usando for of
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdrew ${movement}`);
+//   }
+// }
+movements.forEach((movement, i) => {
+  console.log(
+    `${i + 1}. You ${movement > 0 ? 'deposited' : 'withdrew'} ${movement}`
+  );
+});
+
+console.log('\nMAP');
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const euroToUsd = 1.1;
+
+// Crear un nuevo array a partir de lo que retorna la función callback
+const movementsUSD = movements.map(mov => mov * euroToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+const movDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${mov}`
+);
+console.log(movDescriptions);
+
+console.log('\nFILTER');
+// Crear un nuevo array con todos los elementos que cumplan una condición
+const deposits = movements.filter(mov => mov > 0);
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+console.log('\nREDUCE');
+// Reducir todos los elementos del array a un solo valor
+console.log(movements);
+const balance = movements.reduce((acc, mov) => acc + mov, 0);
+console.log(balance);
+
+// Ejemplo: valor máximo
+const max = movements.reduce((acc, mov) => (acc > mov ? acc : mov), 0);
+console.log(max);
+
+// Usar varios métodos al mismo tiempo
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * euroToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log('\nFIND');
+// Recupera el primer elemento del array que cumpla una condición
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+console.log('\nFINDINDEX');
+// Recupera el índice del primer elemento que cumpla una condición
+
+console.log('\nINCLUDES');
+// Saber si un array contiene un elemento
+console.log(movements.includes(-130));
+console.log(movements.includes(-1308));
+
+console.log('\nSOME');
+// Saber si hay al menos un elemento que cumpla la condición
+console.log(movements.some(mov => mov > 0));
+console.log(movements.some(mov => mov > 5000));
+
+console.log('\nEVERY');
+// Saber si todos los elementos de un array cumplen una condición
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log('\nFLAT');
+// Eliminar arrays anidados y juntar todos los elementos en un solo array
+console.log(arr.flat());
+
+const arrDeep = [[1, [2, 3]], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+
+console.log('\nSORT');
+// Ordenar un array
+const owners = ['Jonas', 'Zach', "Adam", "Martha"];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// Strings
+console.log(owners.sort());
+
+// Numbers
+console.log(movements);
+
+console.log(movements.sort((a,b) => {
+  if (a > b) {
+    return 1;
+  } else if (b > a) {
+    return -1;
+  }
+}));
+
+console.log(movements.sort((a,b) => a - b));
+console.log(movements.sort((a,b) => b - a));
+*/
